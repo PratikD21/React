35e3a15cb851ca3e582afe1b72351fe76f72cf2a
Pratik Darak <darak.pratik@gmail.com>
2019-12-04 19:51:50 +0530

    Updates

---
+++ precache-manifest.79c014f8e7766adc1311dc4d5513974a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c0738bc8c3ffc37626f8cdab55d7ac",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "a12b0dc2e6d92094cc83",
+    "url": "/React/static/css/main.b628c08c.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/React/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "a12b0dc2e6d92094cc83",
+    "url": "/React/static/js/main.39289937.chunk.js"
+  },
+  {
+    "revision": "d24c44b41b7963ad6a48",
+    "url": "/React/static/js/runtime-main.7437e983.js"
+  }
+]);
